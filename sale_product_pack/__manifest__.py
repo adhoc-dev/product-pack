7dece9cf8e0b249c91669788938502085ebbf5e2
Matias Peralta <matias_peralta1@hotmail.com>
2025-04-01 11:42:37 -0300

    [FIX] sale_product_pack: Discount fixed

--- sale_product_pack/__manifest__.py
+++ sale_product_pack/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Product Pack",
-    "version": "16.0.1.0.3",
+    "version": "16.0.1.1.3",
     "category": "Sales",
     "summary": "This module allows you to sell product packs",
     "website": "https://github.com/OCA/product-pack",
